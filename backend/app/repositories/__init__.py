6cab85695e018fb71bda0b359accc1080ae852c8
Gabriel Lopes Bastos <73184283+G4brielLB@users.noreply.github.com>
2026-08-09 20:07:54 -0300

    feat: adiciona repository de horários #33

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,7 @@
+from app.repositories.horario import (
+    buscar_horario_por_id,
+    criar_horario,
+    listar_horarios,
+)
+
+__all__ = ["buscar_horario_por_id", "criar_horario", "listar_horarios"]
